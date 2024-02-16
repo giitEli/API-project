b567689c28277f7505bb2a1ed632054640e381ad
Eli <eli.suffridge@gmail.com>
2024-02-15 18:08:15 -0800

    trying to fix main :c

--- backend/db/seeders/20240215052018-Spots-Seeder.js
+++ backend/db/seeders/20240215052018-Spots-Seeder.js
@@ -23,6 +23,30 @@ module.exports = {
           description: "Place where web developers are created",
           price: 123,
         },
+        {
+          ownerId: 1,
+          address: "123 All Eyes On Me",
+          city: "City of Sin",
+          state: "ShingtonWa",
+          country: "United States of America",
+          lat: 36.7645358,
+          lng: -222.4730327,
+          name: "Good Spot",
+          description: "fake place in my mind",
+          price: 999,
+        },
+        {
+          ownerId: 2,
+          address: "123 fake address",
+          city: "Fake City",
+          state: "Fake State",
+          country: "Fake Country",
+          lat: 69,
+          lng: -69,
+          name: "Fake Place",
+          description: "Fake description for a fake place",
+          price: 9000,
+        },
       ],
       { validate: true }
     );
@@ -34,7 +58,7 @@ module.exports = {
     return queryInterface.bulkDelete(
       options,
       {
-        name: { [Op.in]: ["App Academy"] },
+        name: { [Op.in]: ["App Academy", "Good Spot", "Fake Place"] },
       },
       {}
     );
