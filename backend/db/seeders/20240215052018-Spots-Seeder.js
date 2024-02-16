423ec96d9b0b825d37223dde3ca578bec4cc1863
Eli <eli.suffridge@gmail.com>
2024-02-15 17:57:58 -0800

    trying to fix main :c

--- backend/db/seeders/20240215052018-Spots-Seeder.js
+++ backend/db/seeders/20240215052018-Spots-Seeder.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
+    await Spot.bulkCreate(
       "Spots",
       [
         {
